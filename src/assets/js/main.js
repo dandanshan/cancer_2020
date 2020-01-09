4519d00239e5fb2d7920a94d6271378dac8d032d
dandanshan <shanyuhuan@gmail.com>
2020-01-09 18:31:54 +0800

    [View][CSS][JS] Add top slides & CSS optimize

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
+    $('.burger').click(function(){
+        $(this).toggleClass('active');
+        $('.nav').toggleClass('active');
+    });
 
+    $(window).resize(function(){
+
+        //set top slides dynamically
+        if ($(this).width() < 768) {
+            $('#topslide .slides__top').css('width','100%');
+            $('#topslide .slides__top').height( $(this).width() / 2 );
+        }
+    });
+
+    $('.top__slides').slick({
+        dots: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-prev slides__arrow slides__arrow--gray slides__arrow--prev" aria-label="Previous" type="button"></button>',
+        nextArrow: '<button class="slick-next slides__arrow slides__arrow--gray slides__arrow--next" aria-label="Next" type="button"></button>',
+        dotsClass: 'slides__dots slides__dots--purple',
+    });
+
+    //menuSpy
+    var elm = document.querySelector('.nav');
+    var ms = new MenuSpy(elm);
 
     //mouse follow
     $('body').mousemove(function(e){
@@ -8,4 +34,5 @@ $(document).ready(function(){
             top: e.pageY + 10,
         });
     });
+
 });
